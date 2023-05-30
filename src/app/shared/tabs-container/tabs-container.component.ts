785a7030bcd22301511a1d12fed043292f0acf3d
Abdul Musavveer Alji <abdulmusavveeralji@gmail.com>
2023-05-30 22:38:36 +0530

    tabTitle

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, Component, ContentChildren, QueryList} from '@angular/core';
+import {TabsComponent} from "../tabs/tabs.component";
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabsComponent) tabs: QueryList<TabsComponent> = new QueryList<TabsComponent>();
+  ngAfterContentInit(): void {
+  }
 
 }
