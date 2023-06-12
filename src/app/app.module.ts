728af7326be9d634e0864980c37f11d83adf32db
Abdul Musavveer Alji <abdul.m@ascentbusiness.com>
2023-06-12 09:43:12 +0530

    Bug fixed for Register form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import {UserModule} from "./user/user.module";
-import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -15,8 +14,7 @@ import {ReactiveFormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserModule,
-    ReactiveFormsModule
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
