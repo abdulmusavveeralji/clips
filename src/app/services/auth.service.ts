7cfc59490a7e860e9983c6fe9bf3358f7f2884f5
Abdul Musavveer Alji <abdulmusavveeralji@gmail.com>
2023-07-03 19:38:19 +0530

    User Registration with Update Profile

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/compat/auth";
-import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat/firestore";
 import IUser from "../models/user.models";
 
 @Injectable({
@@ -8,21 +8,35 @@ import IUser from "../models/user.models";
 })
 export class AuthService {
 
+  private usersCollection: AngularFirestoreCollection;
+
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore
-  ) { }
+  ) {
+    this.usersCollection = db.collection<IUser>('users')
+  }
 
   async createUser(userData: IUser) {
+    if (!userData.password) {
+      throw new Error('Password is not provided!')
+    }
     const userCred = await this.auth
       .createUserWithEmailAndPassword(userData.email as string, userData.password as string)
     console.log('userCred', userCred);
 
-    await this.db.collection('users').add({
+    if (!userCred.user) {
+      throw new Error('User cant be found')
+    }
+    await this.usersCollection.doc(userCred.user.uid).set({
       name: userData.name,
       age: userData.age,
       email: userData.email,
       phoneNumber: userData.phoneNumber
     });
+
+    await userCred.user.updateProfile({
+      displayName: userData.name
+  })
   }
 }
